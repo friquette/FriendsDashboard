a7bf03070a406cf10870ee904c72e451c5654bf3
Thomas Fricard <thomasfricard@gmail.com>
2024-09-06 14:51:45 +0200

    route to create a friend

--- backend/routes.py
+++ backend/routes.py
@@ -10,3 +10,29 @@ def get_friends():
     result = [friend.to_json() for friend in friends]
 
     return jsonify(result)
+
+
+@app.route("/api/friends", methods=["POST"])
+def create_friend():
+    try:
+        data = request.json
+
+        required_fields = ["name", "role", "description", "gender"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"error": f"Missing required field: {field}"}), 400
+
+        name = data.get("name")
+        role = data.get("role")
+        description = data.get("description")
+        gender = data.get("gender")
+        img_url = f"https://api.multiavatar.com/{name}.png"
+
+        new_friend = Friend(name=name, role=role, description=description, gender=gender, img_url=img_url)
+        db.session.add(new_friend)
+        db.session.commit()
+
+        return jsonify({"msg": "Friend created successfully !"}), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": 500})
