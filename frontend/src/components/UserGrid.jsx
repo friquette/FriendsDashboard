966939814f308a75178b31ab76d75933635c5d94
Thomas Fricard <thomasfricard@gmail.com>
2024-09-09 17:44:09 +0200

    create user cards

--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Grid } from "@chakra-ui/react"
 
-function UserGrid() {
-  return (
-    <div>UserGrid</div>
-  )
+import UserCard from './UserCard'
+import { USERS } from '../dummy/dummy'
+
+const UserGrid = () => {
+    return <Grid
+        templateColumns={{
+            base: "1fr",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(3, 1fr)"
+        }}
+        gap={"4"}
+    >
+        {USERS.map((user) => (
+            <UserCard key={user.id} user={user} />
+        ))}
+    </Grid>
 }
 
 export default UserGrid
